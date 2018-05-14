62105df15d3c772a5c0e6294eb3e7ebc3ce8e4ad
Meghna Srivastava <meghna.s1906@gmail.com>
2018-05-14 17:48:59 +0200

    made a setTimeOut function

--- src/containers/Timer/index.js
+++ src/containers/Timer/index.js
@@ -8,29 +8,30 @@ class Timer extends Component {
     }
 
     millisToMinutes = () => {
-        const value = this.state.time
-        console.log(value);
-        const seconds = value / 60
-        const minutes = seconds / 1000
-        console.log(minutes);
-
-        this.setState({ time: value })
-    }
-
-    startTimer = () => {
-        const value = this.state.time
-        const seconds = value / 60
-        const minutes = seconds / 1000
+        const value = this.state.time;
+        const seconds = value / 60;
+        const minutes = seconds / 1000;
         console.log(minutes);
 
         this.setState({ defaultTime: `${minutes}:00` })
+        this.startTimer();
+    }
+
+    startTimer() {
+        //write a function of setTimeOut for 25 min 
+        // print some msg after that
+
+        // setTimeout(function, ms);
+
+        const value = this.state.time;
+        setTimeout(() => console.log("Time is up!"), value)
     }
 
     render() {
         return (
-            <Clock 
-            time={this.state.defaultTime}
-            clicked={this.startTimer} />
+            <Clock
+                time={this.state.defaultTime}
+                clicked={this.millisToMinutes} />
         )
     }
 }
